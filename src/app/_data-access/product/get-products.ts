e7d58b542cc1245d254fb02f64f9dc996bc16ad1
Matheus Serafim <matheus18serafim@gmail.com>
2024-11-05 14:37:34 -0300

    feat: Implement caching for product retrieval with unstable_cache and cache functions

--- src/app/_data-access/product/get-products.ts
+++ src/app/_data-access/product/get-products.ts
@@ -2,8 +2,22 @@ import "server-only";
 
 import { db } from "@/app/_lib/prisma-client";
 import { Product } from "@prisma/client";
+import { unstable_cache } from "next/cache";
+import { cache } from "react";
 
-export const getProducts = async (): Promise<Product[]> => {
+export const getProducts = unstable_cache(
+  async (): Promise<Product[]> => {
+    const products = await db.product.findMany();
+    return products;
+  },
+  ["get-products"],
+  {
+    revalidate: 5,
+  },
+);
+
+// independentemente de onde a função getProducts seja chamada, ela sempre retornará os mesmos dados uma unica vez.
+export const getProductsComCache = cache(async (): Promise<Product[]> => {
   const products = await db.product.findMany();
   return products;
-};
+});
