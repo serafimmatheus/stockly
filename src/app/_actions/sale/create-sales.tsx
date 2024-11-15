f185c5cd905f4926342e7431e5797f0c4e6ad2bc
Matheus Serafim <matheus18serafim@gmail.com>
2024-11-15 09:29:30 -0300

    feat: Refactor createSales function to use transaction for improved performance and error handling

--- src/app/_actions/sale/create-sales.tsx
+++ src/app/_actions/sale/create-sales.tsx
@@ -7,53 +7,53 @@ import { revalidateTag } from "next/cache";
 const createSales = async (data: CreateSale) => {
   createSaleSchema.parse(data);
 
-  const sale = await db.sale.create({
-    data: {
-      date: new Date(),
-    },
+  await db.$transaction(async (trx) => {
+    const sale = await trx.sale.create({
+      data: {
+        date: new Date(),
+      },
+    });
+
+    for (const product of data.products) {
+      const productFromDb = await db.product.findUnique({
+        where: {
+          id: product.id,
+        },
+      });
+
+      if (!productFromDb) {
+        throw new Error("Product not found");
+      }
+
+      const productsIsOutOfStock = product.quantity > productFromDb.stock;
+
+      if (productsIsOutOfStock) {
+        throw new Error("Product is out of stock");
+      }
+
+      await trx.saleProduct.create({
+        data: {
+          saleId: sale.id,
+          productId: product.id,
+          quantity: product.quantity,
+          unitPrice: productFromDb.price,
+        },
+      });
+
+      await trx.product.update({
+        where: {
+          id: product.id,
+        },
+        data: {
+          stock: {
+            decrement: product.quantity,
+          },
+        },
+      });
+    }
   });
 
-  for (const product of data.products) {
-    const productFromDb = await db.product.findUnique({
-      where: {
-        id: product.id,
-      },
-    });
-
-    if (!productFromDb) {
-      throw new Error("Product not found");
-    }
-
-    const productsIsOutOfStock = product.quantity > productFromDb.stock;
-
-    if (productsIsOutOfStock) {
-      throw new Error("Product is out of stock");
-    }
-
-    await db.saleProduct.create({
-      data: {
-        saleId: sale.id,
-        productId: product.id,
-        quantity: product.quantity,
-        unitPrice: productFromDb.price,
-      },
-    });
-
-    await db.product.update({
-      where: {
-        id: product.id,
-      },
-      data: {
-        stock: {
-          decrement: product.quantity,
-        },
-      },
-    });
-  }
-
   revalidateTag("get-products");
-
-  return null;
 };
 
 export { createSales };
